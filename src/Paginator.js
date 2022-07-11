cf31dd57c8d75713f30296c3e7523d94a5572299
graynk <gray.nk@gmail.com>
2022-07-11 14:00:16 +0500

    Always display "prev" button to keep "next" from jumping around

--- src/Paginator.js
+++ src/Paginator.js
@@ -7,15 +7,30 @@ var Paginator = React.createClass({
   },
 
   render() {
-    if (this.props.page === 1 && !this.props.hasNext) { return null }
     return <div className="Paginator">
-      {this.props.page > 1 && <span className="Paginator__prev">
-        <Link to={{pathname: `/${this.props.route}`, query: {page: this.props.page - 1}}} onClick={this._onClick}>Prev</Link>
-      </span>}
-      {this.props.page > 1 && this.props.hasNext && ' | '}
-      {this.props.hasNext && <span className="Paginator__next">
-        <Link to={{pathname: `/${this.props.route}`, query: {page: this.props.page + 1}}} onClick={this._onClick}>More</Link>
-      </span>}
+      {
+          this.props.page === 1 ?
+              <span className="Paginator__prev disabled">Prev</span> :
+              <span className="Paginator__prev">
+                <Link
+                    to={{pathname: `/${this.props.route}`, query: {page: this.props.page - 1}}}
+                    onClick={this._onClick}>
+                  Prev
+                </Link>
+              </span>
+      }
+      <span> | </span>
+      {
+          !this.props.hasNext ?
+              <span className="Paginator__next disabled">Next</span> :
+              <span className="Paginator__next">
+                <Link
+                  to={{pathname: `/${this.props.route}`, query: {page: this.props.page + 1}}}
+                  onClick={this._onClick}>
+                  Next
+                </Link>
+              </span>
+      }
     </div>
   }
 })
